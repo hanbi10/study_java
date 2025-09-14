d4a9e15efeacfa5519119a1b8ca267d63d3866a3
hanbi10 <jeonghanbi10@dgsw.hs.kr>
2025-09-14 09:29:12 +0900

    .

---
+++ src/Scanner/Scanner1.java
@@ -0,0 +1,8 @@
+package Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+
+        // Scanner
+    }
+}
---
+++ src/section08/Array5.java
@@ -0,0 +1,17 @@
+package section08;
+
+public class Array5 {
+    public static void main(String[] args) {
+        int[] arr1 = new int[5];
+        arr1[0] = 10;
+        //arr 변수에는 [0,0,0,0,0] 값들이 직접 들어있는게 아니라
+        // . 배열의 참조값(주소값)이 들어있다.
+        // 배열 변수 arr ---(주소값) --> [0,0,0,0,0]
+
+        int[] arr2 = arr1;
+        arr2[0] = 99;
+
+        System.out.println("arr1[0] = " + arr1[0]);
+        System.out.println("arr2[0] = " + arr2[0]);
+    }
+}
