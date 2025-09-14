64ff916c48f2035c02678d7f17991394a396e912
hanbi10 <jeonghanbi10@dgsw.hs.kr>
2025-09-14 09:33:16 +0900

    .

--- src/section08/Array5.java
+++ src/section08/Array5.java
@@ -11,7 +11,17 @@ public class Array5 {
         int[] arr2 = arr1;
         arr2[0] = 99;
 
+        System.out.println(arr1);
+        System.out.println(arr2);
+
         System.out.println("arr1[0] = " + arr1[0]);
         System.out.println("arr2[0] = " + arr2[0]);
+
+        int a = 10;
+        int b = a;
+        b = 20;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
